25edfdb88875a36761473150851fbd872ca34429
Alexandre <xandyid11@gmail.com>
2024-10-12 18:52:49 +0100

    iniciando projeto

---
+++ .Net/Csharp/Estruturas de  repetição/for/Program.cs
@@ -0,0 +1,6 @@
+﻿int numero = 5;
+
+for(int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
---
+++ .Net/Csharp/Estruturas de  repetição/while/Program.cs
@@ -0,0 +1,8 @@
+﻿int numero = 5;
+int contador = 1;
+
+while( contador <=10)
+{
+    Console.WriteLine($"{contador}* Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+}
